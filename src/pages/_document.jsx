ade5a42efbf733c800dc940da8ac71dec5cf0ff9
salahhusa9 <salahhusa9@gmail.com>
2024-04-25 11:56:35 +0100

    Add Google Analytics and Google Ads scripts to _document.jsx

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -1,9 +1,33 @@
 import { Head, Html, Main, NextScript } from 'next/document'
 
+const google = `
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){
+    dataLayer.push(arguments);
+  };
+  gtag('js', new Date());
+
+  gtag('config', 'G-HV1BVMHB0D');
+`
+
+const googleAds = `
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'AW-16543431410');
+`
+
 export default function Document() {
   return (
     <Html className="scroll-smooth bg-white antialiased" lang="en" dir="rtl">
       <Head>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-HV1BVMHB0D"></script>
+        <script dangerouslySetInnerHTML={{ __html: google }} />
+
+        <script async src="https://www.googletagmanager.com/gtag/js?id=AW-16543431410"></script>
+        <script dangerouslySetInnerHTML={{ __html: googleAds }} />
+
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
           rel="preconnect"
